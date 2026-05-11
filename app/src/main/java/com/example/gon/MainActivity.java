e7f953b52cf1ed7f127f26bf21ea9fb0b212f92a
NicksonHe <heisnickson@gmail.com>
2026-05-11 10:54:53 +0200

    nickson test

--- app/src/main/java/com/example/gon/MainActivity.java
+++ app/src/main/java/com/example/gon/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
